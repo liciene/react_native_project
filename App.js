2ec839db6ba61fabc6f42955ac2092e387574471
Liciene <ljcruz7@gmail.com>
2021-02-11 18:03:40 -0300

    URI_1004

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import Basic1001 from './URI_1001';
+// import Basic1002 from './URI_1002';
 // import Basic1003 from './URI_1003';
-import Basic1002 from './URI_1002';
+import Basic1004 from './URI_1004';
 
 const App = () => {
   // return <Basic1001 />;
+  // return <Basic1002 />;
   // return <Basic1003 />;
-  return <Basic1002 />;
+  return <Basic1004 />;
 };
 
 export default App;
