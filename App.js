181cc4c9e1d96bf78b516ca824436de3d3e26499
Liciene Cruz <40743404+liciene@users.noreply.github.com>
2021-02-12 09:17:02 -0300

    URI_1003 (#4)

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-
 import React from 'react';
-import Basic1001 from './URI_1001';
+//import Basic1001 from './URI_1001';
+import Basic1003 from './URI_1003';
 
 const App = () => {
-  return (
-    <Basic1001/>
-  )
-}
+  // return <Basic1001 />;
+  return <Basic1003 />;
+};
 
-export default App
+export default App;
