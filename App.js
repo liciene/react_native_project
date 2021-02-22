6bfb2135b998dd911a6525b73c2987815c523ac2
Liciene Cruz <40743404+liciene@users.noreply.github.com>
2021-02-22 15:43:55 -0300

    URI_1011 (#12)

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import React from 'react';
 // import Basic1007 from './URI_1007';
 // import Basic1008 from './URI_1008';
 // import Basic1009 from './URI_1009';
-import Basic1010 from './URI_1010';
+// import Basic1010 from './URI_1010';
+import Basic1011 from './URI_1011';
 
 const App = () => {
   // return <Basic1001 />;
@@ -20,7 +21,8 @@ const App = () => {
   // return <Basic1007 />;
   // return <Basic1008 />;
   // return <Basic1009 />;
-  return <Basic1010 />;
+  // return <Basic1010 />;
+  return <Basic1011 />;
 };
 
 export default App;
